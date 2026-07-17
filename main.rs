c122f172ab5ad65c5220e732dac15a7d13d7c009
Arjit Rajvanshi <arjitsystem03@gmail.com>
2026-07-17 17:50:57 +0530

    done

--- main.rs
+++ main.rs
@@ -1,8 +1,14 @@
 use std::io;
 
 fn main() {
-    let mut input: String = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    input = input.trim().to_uppercase();
-    println!("{}", input);
+    let mut name: String = String::new();
+    io::stdin().read_line(&mut name).unwrap();
+    name = name.trim().to_string();
+
+    let mut age: String = String::new();
+    io::stdin().read_line(&mut age).unwrap();
+
+    let age: u32 = age.trim().parse().unwrap();
+
+    println!("Hi, {name}! You are {age} years old.");
 }
