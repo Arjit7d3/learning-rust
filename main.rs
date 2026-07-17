c7fe16341bbb06f07cda468977bcc5c913687684
Arjit Rajvanshi <arjitsystem03@gmail.com>
2026-07-17 17:14:36 +0530

    done

--- main.rs
+++ main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, Rust!");
+    let a: i32;
+    let b: i32;
+
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    a = input.trim_end().parse().unwrap();
+
+    input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    b = input.trim_end().parse().unwrap();
+
+    println!("{}", a + b);
 }
