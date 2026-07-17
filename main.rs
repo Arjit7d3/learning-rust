f1a1142daa35078c68c115a8100a0ae05deb2bbd
Arjit Rajvanshi <arjitsystem03@gmail.com>
2026-07-17 17:44:07 +0530

    done

--- main.rs
+++ main.rs
@@ -3,11 +3,6 @@ use std::io;
 fn main() {
     let mut input: String = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let a: i32 = input.trim().parse().unwrap();
-
-    input.clear();
-    io::stdin().read_line(&mut input).unwrap();
-    let b: i32 = input.trim().parse().unwrap();
-
-    println!("{}", a * b);
+    input = input.trim().to_uppercase();
+    println!("{}", input);
 }
