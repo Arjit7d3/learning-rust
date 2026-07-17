1b93b773830e170ce8a786ff2f2e2958a98eadf6
Arjit Rajvanshi <arjitsystem03@gmail.com>
2026-07-17 17:19:57 +0530

    done

--- main.rs
+++ main.rs
@@ -3,11 +3,11 @@ use std::io;
 fn main() {
     let mut input: String = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let a = input.trim().parse().unwrap();
+    let a: i32 = input.trim().parse().unwrap();
 
     input.clear();
     io::stdin().read_line(&mut input).unwrap();
-    let b = input.trim().parse().unwrap();
+    let b: i32 = input.trim().parse().unwrap();
 
     println!("{}", a + b);
 }
